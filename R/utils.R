4c133ddc5b23960f31d8d05508f3cfdf886a4add
Pieter Marres <pieter.marres@eneco.com>
2019-08-14 21:53:21 +0200

    Added lookup function

--- R/utils.R
+++ R/utils.R
@@ -25,3 +25,27 @@ csv2rds <- function(fcsv){
   return(NULL)
 }
 
+
+#' vlookup
+#'
+#' Lookup function similar to Excel but with two extra features: lookup column
+#' can be any column (not just the 1st column) and includes interpolation
+#' lookup
+#'
+#'
+#' @param lookup_value a length-one vector, value you want to lookup
+#' @param table dataframe specifying the lookup table
+#' @param lookup_colname a string, column name containing lookup_value
+#' @param return_colname a string, column name containing return value
+#' @param range_lookup a string indicating lookup method,
+#' either "floor", "interpolate" or "ceiling"
+#' @return a length-one vector, specifying the value found in column labeled
+#' with given lookup_colname
+#' @export
+#'
+#' @examples
+#'
+#'
+vlookup <- function(){
+
+}
