3e9e170e3e5bd8486ac50404da623e15308edce9
Pieter Marres <info@oxylo.com>
2019-08-17 20:20:43 +0200

    Understanding the apply family even better

--- R/experiment.R
+++ R/experiment.R
@@ -1,28 +1,69 @@
 ## understanding the *apply family
 
-## apply()
+source("utils.R")
 
+## apply() : matrix --> vector
+
+print("--- apply data ---")
 m <- matrix(c(1,0,1,1,0,
               1,1,1,1,0,
               1,0,0,1,1,
               0,1,1,1,0), nrow = 4)
+print(m)
+
+print("--- apply output ---")
+v1 <- apply(m, MARGIN = 1, copymay, d=3)
+print(v1)
+v2 <- apply(m, MARGIN = 2, copymay, d=4)
+print(v2)
+
+## lapply : list --> list (also works on dataframes and vectors)
+print("--- apply data ---")
+lst <- list(1:3, c(1, 6, 100), seq(1, 3, 0.5))
+print(lst)
+
+print("--- apply output ---")
+lst1 <- lapply(lst, median)
+print(lst1)
+lst2 <- lapply(lst, "+", 2)
+print(lst2)
+lst3 <- lapply(lst, "*", 1000)
+print(lst3)
+
+## sapply (SIMPLE apply: list --> vector
+print("--- sapply data ---")
+lst <- list((1:3), c(1, 6, 100), seq(1, 3, 0.5))
+print(lst)
+
+print("--- sapply output ---")
+lst1 <- sapply(lst, median)
+print(lst1)
+lst2 <- sapply(lst, "+", 2)  # same output as lapply
+print(lst2)
+lst3 <- sapply(lst, "*", 1000)  # same output as lapply
+print(lst3)
 
 
-#' Copymay
-#'
-#' Checks mayority of ones in first d vector elements
-#'
-#' @param x vector containing zeros and ones only
-#' @param d integer indicating how many elements should be taken into account
-#'
-#' @return Boolean. TRUE if most d elements are 1, else FALSE
-#' @export
-#'
-#' @examples
-#' copymay(c(1, 0, 1), 2)  # FALSE
-#' copymay(c(1, 0, 1), 3)  # TRUE
-#' copymay(c(1, 0, 1, 0), 4)  # FALSE
-copymay <- function(x, d){
-  items <- x[1:d]
-  return(mean(items) > 0.5)
-}
+## tapply: vector (+factor) --> vector
+print("--- tapply data ---")
+salary <- sample(1000 * c(35, 42, 50, 57, 63, 72, 107, 167), size=10, replace=TRUE)
+fact <- list(gender=c(rep("M", 5), rep("F", 5)),
+             eye_color=rep(c("blue", "brown"), 5))
+print(data.frame(list(salary=salary, gender=fact["gender"], eye_color=fact["eye_color"])))
+print("--- tapply output ---")
+tab <- tapply(salary, INDEX=fact, FUN=mean)
+print(tab)
+
+
+## mapply (MULTIVARIATE apply): vectorizes functions that are by itself are not vectorized
+
+outp <- mapply(rep, 1:3, 5)
+print(outp)
+
+
+
+
+
+
+
+
